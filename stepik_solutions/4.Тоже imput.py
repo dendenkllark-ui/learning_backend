817fca72474c3e6261d4bcefc3fec75b5fdf7aa5
Daniil Moller <dendenkllark@gmail.com>
2026-04-20 21:21:14 +0700

    Завершил модуль 2.3 на степике

---
+++ stepik_solutions/4.Тоже imput.py
@@ -0,0 +1,15 @@
+name = input()
+print("Привет,", name)
+
+team = input()
+print(team, "- чемпион!")
+
+a = input()
+b = input()
+c = input()
+print(a, b, c, sep="\n")
+
+a = input()
+b = input()
+c = input()
+print(c, b, a, sep="\n")
