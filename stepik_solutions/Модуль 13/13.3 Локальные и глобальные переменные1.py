31f2e037b6cd978ae9912a318a37f4aa38682878
Daniil Moller <dendenkllark@gmail.com>
2026-07-06 22:24:27 +0700

    Прошел раздел 13.3 на степике

---
+++ stepik_solutions/Модуль 13/13.3 Локальные и глобальные переменные1.py
@@ -0,0 +1,43 @@
+# # Локальные переменные
+# def print_texas():
+#     birds = 5000
+#     print('В Техасе обитает', birds, 'птиц.')
+
+# def print_california():
+#     birds = 9000
+#     print('В Калифорнии обитает', birds, 'птиц.')
+
+
+# # Глобальные переменные
+# birds = 5000   # глобальная переменная
+
+# def print_texas():
+#     birds = 1000  # локальная переменная
+#     print('В Техасе обитает', birds, 'птиц.')
+
+# def print_california():
+#     birds = 7000  # локальная переменная
+#     print('В Калифорнии обитает', birds, 'птиц.')
+
+# # Ключевое слово global
+# def print_texas():
+#     global birds
+#     birds = 5000
+#     print('В Техасе обитает', birds, 'птиц.')
+
+# def print_california():
+#     print('В Калифорнии обитает', birds, 'птиц.')
+
+# print_texas()
+# print_california()
+
+x = 5
+
+def add():
+    global x
+    x = 3
+    x = x + 5
+    print(x)
+
+add()
+print(x)
